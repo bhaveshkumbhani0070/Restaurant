ce00e6f5390d9d926f85f2708d2e010f14c89f3f
kumbhanialex111 <kumbhani.bhavesh.1@gmail.com>
2018-04-11 09:17:47 +0530

    [WP] CONTACT US update for contact us page

--- src/pages/contact-us/contact-us.ts
+++ src/pages/contact-us/contact-us.ts
@@ -1,5 +1,8 @@
+// import { EnquiryProvider } from '../../providers/enquiry/enquiry';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, PopoverController, ToastController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+// HttpHeaders
 
 /**
  * Generated class for the ContactUsPage page.
@@ -15,11 +18,100 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactUsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  model: any = {}
+  loading:any;
+  tokenoneSignal:string= 'MDdmN2U1NjQtY2MwZS00YWEyLWIxMmItNWNhMGE1MmM2NjRm';	
+  
+  
+  constructor(
+    public http: HttpClient,
+    private loadingCtrl: LoadingController , 
+    private toastCtrl: ToastController , 
+    private popoverCtrl: PopoverController, 
+    // private enquiry: EnquiryProvider
+  ) {
+    this.model = {
+                name: '',
+                email: '',
+                txt: ''
+            }
+          
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactUsPage');
+    console.log('ionViewDidLoad EnquiryPage');
+  }
+
+  optionsPopover(event) {
+    let popover = this.popoverCtrl.create('PopPage')
+       popover.present({
+          ev: event
+       });
+  }
+
+  sendEnquiry(){
+    this.showLoader();    
+    // this.enquiry.sendEnquiry(this.model)
+    //   .subscribe(
+    //       data => {
+    //         this.model = {
+    //           name: '',
+    //           email: '',
+    //           txt: ''
+    //       }
+    //       this.loading.dismiss()        
+          
+    //       this.presentToast('Message Sent Successfuly')
+    //       this.sendPush()
+    //       },
+    //       error => {
+    //         this.loading.dismiss()                    
+    //         this.presentToast('Something went wrong try again!');
+    //         console.log(error.status)
+    //   });
+  }
+
+  showLoader(){
+    this.loading = this.loadingCtrl.create({
+        content: ''
+    });
+    this.loading.present();
+  }
+
+  presentToast(data) {
+    let toast = this.toastCtrl.create({
+      message: data,
+      duration: 4000,
+      position: 'bottom',
+    });
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+
+  sendPush(){
+    console.log('sent pushing')
+    // let username = localStorage.getItem('username');    
+    // var headers = new HttpHeaders();
+    // headers.append('Authorization', 'Basic ' + this.tokenoneSignal); 
+    // let item = {
+    //   "app_id": "2816ecdb-566e-4ee9-bcb3-f9257aaf04a8",
+    //     "contents": {"en": "We have received your enquiry, your flight valet will contact you shortly."},
+    //     "filters": [
+    //         {"field": "tag", "key": "Customer_Id", "relation": "=", "value": username}
+    //     ]
+    // }
+
+    // this.http.post('https://onesignal.com/api/v1/notifications', item, {
+    //   headers: new HttpHeaders().set('Authorization', `Basic ${this.tokenoneSignal}`)
+    // } )
+    // .subscribe(res => {
+    //   console.log(res)
+    // }, (err) => {
+    //  console.log(err)
+    // });  
   }
 
 }
